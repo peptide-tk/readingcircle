ed166ce64d7f489fb7bb1f58de6991cce8c82e11
peptide-tk <nonjdlc@gmail.com>
2025-05-31 19:04:40 +0900

    do rubocop

--- app/models/subtask.rb
+++ app/models/subtask.rb
@@ -1,4 +1,4 @@
 class Subtask < ApplicationRecord
   belongs_to :assignment
-  belongs_to :assigned_user, class_name: 'User', optional: true
+  belongs_to :assigned_user, class_name: "User", optional: true
 end
