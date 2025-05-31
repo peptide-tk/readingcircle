e6476e4b4757fb4b58a99d9faf71b47135c6628b
peptide-tk <nonjdlc@gmail.com>
2025-05-31 21:32:20 +0900

    feat: seed追加追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+group = Group.create!(name: "テストグループ")
+
+User.create!(
+  email: "test@example.com",
+  password: "password",
+  password_confirmation: "password",
+  group_id: group.id
+)
